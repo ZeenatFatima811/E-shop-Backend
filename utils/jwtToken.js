9859c6cac04efe4f317fc2c375319e3b8c76daa4
Currency-Converter <z8784507@gmail.com>
2026-07-04 11:00:22 +0500

    Changing jwt code

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,17 +1,40 @@
-// create token and saving that in cookies
-const sendToken =(user, statusCode, res) =>{
-    const token= user.getJwtToken();
+// // create token and saving that in cookies
+// const sendToken =(user, statusCode, res) =>{
+//     const token= user.getJwtToken();
 
-    //options for cookies
-    const options= {
-        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-    };
-    res.status(statusCode).cookie("token", token, options).json({
-        success: true,
-        user, 
-        token,
+//     //options for cookies
+//     const options= {
+//         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+//         httpOnly: true,
+//     };
+//     res.status(statusCode).cookie("token", token, options).json({
+//         success: true,
+//         user, 
+//         token,
+//     });
+// };
+
+// module.exports= sendToken;
+
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+
+  const options = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  };
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      user,
+      token,
     });
 };
 
-module.exports= sendToken;
+module.exports = sendToken;
